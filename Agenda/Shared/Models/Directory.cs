41ce20dc497de5cac2145f0892eae4a9fc8194b0
aperezcsi <99925195+aperezcsi@users.noreply.github.com>
2022-02-24 16:29:42 -0500

    search update

---
+++ Agenda/Shared/Models/Directory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Agenda.Shared.Models
+{
+    public class Directory
+    {
+        [Key]
+        [Required(ErrorMessage = "El tamaño es requerido")]
+        public string size { get; set; }
+    }
+}
